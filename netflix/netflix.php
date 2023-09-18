a308af86b429a9512954806239778f0e66a39514
sahilraj-001x <84084455+sahilraj-001x@users.noreply.github.com>
2023-09-18 22:30:49 +0530

    Update netflix.php

--- netflix/netflix.php
+++ netflix/netflix.php
@@ -69,9 +69,6 @@
                 </div>
             </div>
         </div>
-        <?php
-        $destination = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        require_once('helper.php');
-        ?>
+        
     </body>
 </html>
